4c0ab517d3f6f92e4a15dd92ae134932f89fabc4
Sander Bjerklund Lindberg <sander.b.lindberg@gmail.com>
2021-10-11 12:14:05 +0200

    Oversampling

--- dataset.py
+++ dataset.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from imblearn.over_sampling import SMOTE
 from collections import namedtuple
+from sklearn.inspection import permutation_importance
 from imblearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, RobustScaler, LabelEncoder
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, IterativeImputer
 from sklearn.model_selection import GridSearchCV
@@ -19,23 +21,28 @@ from catboost import CatBoostClassifier
 class Dataset:
     
     hex_columns = ['f2', 'f3', 'f13', 'f18', 'f20', 'f26']
-    bool_columns = ['f28', 'f22', 'f14']
     ordinal_columns = ['f6', 'f8', 'f16', 'f17', 'f19', 'f21', 'f25']
     categorical_columns = ['f1', 'f5', 'f7', 'f9', 'f11', 'f24']
     numerical_columns = ['f4']
     alphabet_columns = ['f0']
     ordinal_cat_columns = ['f0', 'f10', 'f12', 'f23', 'f27']
-    binary_columns = ['f10']
+    binary_columns = ['f10', 'f28', 'f22', 'f14']
     all_columns = ['f' + str(i) for i in range(0, 28)]
     removed_cols = []
 
     def __init__(self, train_path : str, test_path : str) -> None:
         self.train_path = train_path
         self.test_path = test_path
-        self.df = None
+        self.df_train = None
+        self.df_test = None
         self.X_train = None
         self.X_test = None
         self.y = None
+
+        self.numeric_with_outliers_transformer = make_pipeline(
+            IterativeImputer(random_state=42),
+            RobustScaler()
+        )
 
         self.numeric_transformer = make_pipeline(
             IterativeImputer(random_state=42),
@@ -52,35 +59,44 @@ class Dataset:
             OrdinalEncoder()
         )
 
-        numerical_transformer_cols = self.numerical_columns + self.hex_columns + self.bool_columns + self.ordinal_columns
-        categorical_transformer_cols = self.categorical_columns + self.alphabet_columns
+
+        numerical_transformer_cols = self.numerical_columns + self.hex_columns + self.ordinal_columns
+        categorical_transformer_cols = self.categorical_columns
 
         categorical_transformer_cols.remove('f11')
+        # numerical_transformer_cols.remove('f4')
 
         self.preprocessor = make_column_transformer(
             (self.numeric_transformer, numerical_transformer_cols),
+            # (self.numeric_with_outliers_transformer, ['f4']),
             (self.categorical_transformer, categorical_transformer_cols),
-            (self.ordinal_transformer, self.ordinal_cat_columns)
+            (self.ordinal_transformer, self.ordinal_cat_columns + self.binary_columns),
         )
 
     def load(self) -> tuple:
         self.load_train(self.train_path)
+        self.balance()
         self.load_test(self.test_path)
-
 
         return self.X_train, self.y, self.X_test
 
+    def balance(self, test=True):
+        sm = SMOTE(sampling_strategy='minority', random_state=7)
+
+        self.X_train, self.y = sm.fit_resample(self.X_train, self.y)
+
+
     def load_train(self, dataset : str, delimiter = ',') -> None:
-        self.df = pd.read_csv(dataset, delimiter=delimiter)
-        self.X_train = self.df.drop(columns=['id', 'target'])
-        self.y = self.df['target']
-        self.X = self.df.drop(columns=['id', 'target'])
+        self.df_train = pd.read_csv(dataset, delimiter=delimiter)
+        self.X_train = self.df_train.drop(columns=['id', 'target'])
+        self.y = self.df_train['target']
+        self.X = self.df_train.drop(columns=['id', 'target'])
 
         self.preprocess_train(self.X_train)
 
     def load_test(self, dataset : str, delimiter = ',') -> None:
-        self.df = pd.read_csv(dataset, delimiter = delimiter)
-        self.X_test = self.df.drop(columns=['id'])
+        self.df_test = pd.read_csv(dataset, delimiter = delimiter)
+        self.X_test = self.df_test.drop(columns=['id'])
 
         self.preprocess_test(self.X_test)
 
@@ -97,6 +113,7 @@ class Dataset:
         
         self.conv_columns(df)
         # self.fillna(df)
+        # self.remove_rows(df)
         self.transform(df, test)
                 
     def remove_duplicate_columns(self, df : pd.DataFrame) -> None:
@@ -148,11 +165,11 @@ class Dataset:
             except Exception as e:
                 return np.nan
         
-        for col in self.bool_columns:
-            if col not in list(df.columns):
-                continue
-            col_loc = list(df.columns).index(col)
-            df[col] = df[col].apply(lambda x: conv_bool_map(x))
+        # for col in self.ordinal_cat_columns:
+        #     if col not in list(df.columns):
+        #         continue
+        #     col_loc = list(df.columns).index(col)
+        df['f14'] = df['f14'].apply(lambda x: conv_bool_map(x))
 
     def conv_binary(self, df : pd.DataFrame):
         def binaryToDecimal(binary):
@@ -187,9 +204,18 @@ class Dataset:
             # print(self.X_train.head())
             self.X_train = pd.DataFrame(self.preprocessor.fit_transform(self.X_train, self.y))
             print(pd.DataFrame({'total_missing': self.X_train.isnull().sum(), 'perc_missing': (self.X_train.isnull().sum()/50000)*100}))
-    
+
     def remove_duplicates(self, df : pd.DataFrame) -> None:
         df.drop_duplicates(inplace=True)
+
+    def remove_rows(self, df):
+        df = df[~df['f27'].isin(['g', 'i', 'j'])]
+        df = df[~df['f23'].isin(['B', 'C'])]
+        df = df[~df['f17'].isin([0.2, 0.3])]
+        df = df[~df['f16'].isin([10])]
+        df = df[~df['f0'].isin(['W', 'Z', 'S', 'G', 'V', 'J', 'L'])]
+        df = df[~df['f8'].isin([2.0, 3.0])]
+
         
 
 def split(X : pd.DataFrame, y : pd.DataFrame, size=.2):
@@ -244,15 +270,16 @@ if __name__ == '__main__':
     
     X_train, y, X_test = dataset.load()
     
+
     X_train_split, X_test_split, y_train_split, y_test_split = split(X_train, y)
 
     # clf = get_classifier('adaboost')
     classifiers = {
-        'adaboost': AdaBoostClassifier(n_estimators=100, random_state=0),
+        'adaboost': AdaBoostClassifier(n_estimators=1000),
         'gnb': GaussianNB(),
         'lgbm': LGBMClassifier(),
         'rf': RandomForestClassifier(n_estimators=100),
-        'catboost': CatBoostClassifier(iterations=1000, learning_rate=1, depth=1)
+        'catboost': CatBoostClassifier(iterations=10000, learning_rate=1, depth=1)
     }
 
     param_grid = {
@@ -279,17 +306,18 @@ if __name__ == '__main__':
     }
     # run grid search
     LGBM = LGBMClassifier()
-    clf = GridSearchCV(LGBM, param_grid=lgbm_params, scoring = 'roc_auc')
-
-    # clf = StackingClassifier(estimators = list(classifiers.items()), final_estimator = CatBoostClassifier(iterations=10000, learning_rate=.1, depth=2))
+    # clf = GridSearchCV(LGBM, param_grid=lgbm_params, scoring = 'roc_auc')
+    clf = CatBoostClassifier(depth = 1, iterations = 10000, learning_rate = 1)
+    # clf = AdaBoostClassifier(n_estimators=1000)
+    # clf = StackingClassifier(estimators = [('ada', classifiers['adaboost']), ('lgbm', classifiers['lgbm'])], final_estimator = classifiers['catboost'])
     # clf = classifiers['catboost']
     # print('Starting prediction')
     fit(X_train_split, y_train_split, clf)
     # print('Prediction done')
-    print(clf.best_params_) 
+    # print(clf.best_params_) 
     prediction = predict_proba(clf, X_test_split)
 
-    print_accuracy(prediction, y_test_split, 'grid')
+    print_accuracy(prediction, y_test_split, 'Catboost')
 
     real_pred = predict_proba(clf, X_test)
 
