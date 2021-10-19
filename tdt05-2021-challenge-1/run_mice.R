3ca307cdbb85fa218fea87060d7c7ee08860ef99
Sander Lindberg <sander.b.lindberg@gmail.com>
2021-10-19 08:08:03 +0200

    mice

--- tdt05-2021-challenge-1/run_mice.R
+++ tdt05-2021-challenge-1/run_mice.R
@@ -1,12 +1,10 @@
 library(mice)
 
 args <- commandArgs(trailingOnly=TRUE)
-# infile = args[1]
-# outfile = args[2]
-infile = "/Users/sanderlindberg/Documents/TDT05/tdt05-2021-challenge-1/train.csv"
-outfile = "/Users/sanderlindberg/Documents/TDT05/tdt05-2021-challenge-1/train_imputed.csv"
+infile = args[1]
+outfile = args[2]
 
 data_with_nan <- read.csv(infile, sep=",")
-data <- complete(mice(data_with_nan, printFlag=FALSE))
+data <- complete(mice(data_with_nan, printFlag=FALSE, method = "cart"))
 
 write.csv(data, file=outfile, row.names=FALSE, col.names=FALSE, sep=",")
---
+++ tdt05-2021-challenge-1/run_mice3.r
@@ -0,0 +1,11 @@
+library(mice)
+
+
+args <- commandArgs(trailingOnly=TRUE)
+infile = args[1]
+outfile = args[2]
+
+data_with_nan <- read.csv(infile, sep=",")
+data <- complete(mice(data_with_nan, printFlag=FALSE))
+
+write.csv(data, file=outfile, row.names=FALSE, col.names=FALSE, sep=",")
